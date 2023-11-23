c10967d014e6cec8189175794f2f5b4e69cf772f
Sherlyn <sherlynballestero@gmail.com>
2023-11-22 20:27:28 -0500

    added things

--- gui.py
+++ gui.py
@@ -1,45 +1,51 @@
 import tkinter as tk
 from tkinter import ttk
 import lcs
-from PIL import ImageTk, Image
 
+# Función para cambiar de ventana
+def cambiar_ventana(principalWindow, newWindow):
+    principalWindow.withdraw()  # Ocultar la ventana actual
+    newWindow.deiconify()  # Mostrar la ventana secundaria
 
-
-
-#Cambiar de ventana
-def cambiar_ventana(principalWindows,newWindows):
-    principalWindows.withdraw()  # Ocultar la ventana actual
-    newWindows.deiconify()  # Mostrar la ventana secundaria
-
-# Crear la ventana secundaria1
-window2 = tk.Tk()
-# Crear la ventana secundaria1
-window3 = tk.Tk()
-#pantalla principal 
-window=tk.Tk()
+# Crear la ventana principal
+window = tk.Tk()
 window.title("Proyecto de Longest Common Subsequence")
 window.geometry("600x400")
 window.configure(bg="light blue")
-# # Crear botón en la ventana principal
-boton1 = tk.Button(window, text="mayor subsecuencia común ", command=lambda: cambiar_ventana(window, window2),bg = "gray",fg="white")
+
+# Crear botón en la ventana principal para abrir ventana2
+boton1 = tk.Button(window, text="Mayor subsecuencia común", command=lambda: cambiar_ventana(window, window2), bg="gray", fg="white")
 boton1.pack(pady=40)
-boton2 = tk.Button(window, text="Plagio entre documentos", command=lambda: cambiar_ventana(window, window3),bg = "gray",fg="white")
+
+# Crear botón en la ventana principal para abrir ventana3
+boton2 = tk.Button(window, text="Plagio entre documentos", command=lambda: cambiar_ventana(window, window3), bg="gray", fg="white")
 boton2.pack(pady=20)
 
-#definicion ventana2
-window2.title("mayor subsecuencia común entre dos cadenas")
-window2.geometry("600x400")
+# Crear botón en la ventana principal para abrir ventana4
+boton3 = tk.Button(window, text="Mayor subsecuencia palíndroma", command=lambda: cambiar_ventana(window, window4), bg="gray", fg="white")
+boton3.pack(pady=20)
 
-# Crear los widgets
-label1 = tk.Label(window2, text="Ingrese la primera cadena:",bg="light blue")
-entry1 = tk.Entry(window2)
-label2 = tk.Label(window2, text="Ingrese la segunda cadena:",bg="light blue")
-entry2 = tk.Entry(window2)
-button = tk.Button(window2, text="Encontrar la mayor subsecuencia común", command=lambda: result.config(text=lcs.lcs(entry1.get(), entry2.get())),bg = "gray",fg="white")
-label3 = tk.Label(window2, text="La mayor subsecuencia común es:",bg="light blue")
-result = tk.Label(window2, text="")
+# Crear botón en la ventana principal para abrir ventana5
+boton3 = tk.Button(window, text="Mayor subsecuencia en varias cadenas", command=lambda: cambiar_ventana(window, window5), bg="gray", fg="white")
+boton3.pack(pady=20)
+
+# Definición de ventana2
+window2 = tk.Toplevel(window)
+window2.title("Mayor subsecuencia común entre dos cadenas")
+window2.geometry("600x400")
 window2.configure(bg="light blue")
-# Colocar los widgets en la ventana secundaria
+
+# Crear los widgets en la ventana2
+label1 = tk.Label(window2, text="Ingrese la primera cadena:", bg="light blue")
+entry1 = tk.Entry(window2)
+label2 = tk.Label(window2, text="Ingrese la segunda cadena:", bg="light blue")
+entry2 = tk.Entry(window2)
+button = tk.Button(window2, text="Encontrar la mayor subsecuencia común", command=lambda: result.config(text=lcs.find_longest_palindromic_subsequence(entry1.get(), entry2.get())), bg="gray", fg="white")
+label3 = tk.Label(window2, text="La mayor subsecuencia común es:", bg="light blue")
+result = tk.Label(window2, text="")
+boton5 = tk.Button(window2, text="Atrás", command=lambda: cambiar_ventana(window2, window), bg="gray", fg="white")
+
+# Colocar los widgets en la ventana2
 label1.pack(pady=20)
 entry1.pack()
 label2.pack(pady=10)
@@ -47,37 +53,78 @@ entry2.pack()
 button.pack(pady=10)
 label3.pack(pady=10)
 result.pack(pady=10)
-boton5=tk.Button(window2,text="atras", command=lambda: cambiar_ventana(window2, window),bg = "gray",fg="white")
 boton5.pack(pady=30)
-window2.withdraw()
 
-#definicion ventana secundaria2
+# Definición de ventana3
+window3 = tk.Toplevel(window)
 window3.title("Plagio entre documentos")
 window3.geometry("600x400")
-window3.withdraw()
 window3.configure(bg="light blue")
-#crear combobox
-opciones = ["contents/Abecedario.txt", "contents/AbecedarioCopia.txt", "contents/Arianne.txt","contents/Choque de reyes.txt"]
-lb = tk.Label(window3, text="Seleccione el documento1",bg="light blue")
-lb.pack(pady=20)
-combobox1 = ttk.Combobox(window3, values=opciones)
-combobox1.pack(pady=10)
-valorSeleccionado1=combobox1.get()
-lb2 = tk.Label(window3, text="Seleccione el documento2",bg="light blue")
-lb2.pack(pady=10)
-combobox2=ttk.Combobox(window3, values=opciones)
-combobox2.pack(pady=10)
-valorSeleccionado2=combobox2.get()
-def seleccionarOpcion():
-    valorSeleccionado1 = combobox1.get()
-    valorSeleccionado2 = combobox2.get()
-    resultb3.config(text=lcs.plagiarism_check(str(valorSeleccionado1), str(valorSeleccionado2)))
 
-boton3 = tk.Button(window3, text="Detectando Plagio", command=lambda: seleccionarOpcion(),bg = "gray",fg="white")
-boton3.pack(pady=20)
+# Crear combobox en la ventana3
+opciones = ["contents/Abecedario.txt", "contents/AbecedarioCopia.txt", "contents/Arianne.txt", "contents/Choque de reyes.txt"]
+lb = tk.Label(window3, text="Seleccione el documento 1", bg="light blue")
+combobox1 = ttk.Combobox(window3, values=opciones)
+lb2 = tk.Label(window3, text="Seleccione el documento 2", bg="light blue")
+combobox2 = ttk.Combobox(window3, values=opciones)
+boton3 = tk.Button(window3, text="Detectando Plagio", command=lambda: resultb3.config(text=lcs.plagiarism_check(combobox1.get(), combobox2.get())), bg="gray", fg="white")
 resultb3 = tk.Label(window3, text="")
+boton4 = tk.Button(window3, text="Atrás", command=lambda: cambiar_ventana(window3, window), bg="gray", fg="white")
+
+# Colocar los widgets en la ventana3
+lb.pack(pady=20)
+combobox1.pack(pady=10)
+lb2.pack(pady=10)
+combobox2.pack(pady=10)
+boton3.pack(pady=20)
 resultb3.pack(pady=20)
-boton4=tk.Button(window3,text="atras", command=lambda: cambiar_ventana(window3, window),bg = "gray",fg="white")
 boton4.pack(pady=20)
+
+# Definición de ventana4
+window4 = tk.Toplevel(window)
+window4.title("Mayor subsecuencia palíndroma")
+window4.geometry("600x400")
+window4.configure(bg="light blue")
+
+# Crear los widgets en la ventana4
+l1 = tk.Label(window4, text="Ingrese la primera cadena:", bg="light blue")
+e1 = tk.Entry(window4)
+b = tk.Button(window4, text="Encontrar la mayor subsecuencia palíndroma", command=lambda: r.config(text=lcs.longest_palindrome_subsequence(e1.get())), bg="gray", fg="white")
+l3 = tk.Label(window4, text="La mayor subsecuencia palindómica es:", bg="light blue")
+r = tk.Label(window4, text="")
+b5 = tk.Button(window4, text="Atrás", command=lambda: cambiar_ventana(window4, window), bg="gray", fg="white")
+
+# Colocar los widgets en la ventana4
+l1.pack(pady=20)
+e1.pack()
+b.pack(pady=10)
+l3.pack(pady=10)
+r.pack(pady=10)
+b5.pack(pady=30)
+
+# Definición de ventana5
+window5 = tk.Toplevel(window)
+window5.title("Mayor subsecuencia en varias cadenas")
+window5.geometry("600x400")
+window5.configure(bg="light blue")
+# Crear los widgets en la ventana5
+l51 = tk.Label(window5, text="Ingrese las cadenas que quiera analizar separadas por coma:", bg="light blue")
+e51 = tk.Entry(window5)
+b52=tk.Button(window5,text="ok",command=lambda:r5.config(text=lcs.LCS_inmultiplesStrings(e51.get())), bg="gray", fg="white")
+r5 = tk.Label(window5, text="")
+b51 = tk.Button(window5, text="Atrás", command=lambda: cambiar_ventana(window5, window), bg="gray", fg="white")
+# Colocar los widgets en la ventana5
+l51.pack(pady=20)
+e51.pack(pady=20)
+b52.pack(pady=20)
+b51.pack(pady=20)
+r5.pack(pady=20)
+
+# Ocultar las ventanas secundarias al inicio
+window2.withdraw()
+window3.withdraw()
+window4.withdraw()
+window5.withdraw()
+
 # Iniciar el bucle principal de la ventana
 window.mainloop()
--- lcs.py
+++ lcs.py
@@ -3,29 +3,93 @@ import os
 
     
     #Function to find the length of longest common subsequence in two strings.
-def lcs(s1,s2):
-    x = len(s1)
-    y = len(s2)
-    dp = [[0 for i in range(y + 1)] for j in range(x + 1)]
-    for i in range (1, x + 1):
-        for j in range (1, y + 1):
-            if s1[i - 1] == s2[j - 1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+def lcs(s1,s2):  
+    x = len(s1) 
+    y = len(s2)  
+    dp = [[0 for i in range(y + 1)] for j in range(x + 1)] 
+    for i in range (1, x + 1):  
+        for j in range (1, y + 1):  
+            if s1[i - 1] == s2[j - 1]:   
+                dp[i][j] = dp[i-1][j-1] + 1   
+            else: 
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
     
-    i = x
-    j = y
-    z = ""
-    while i>0 and j > 0:
-        if s1[i - 1] == s2[j - 1]:
-            z = s1[i - 1] + z
-            i -= 1
+    i = x 
+    j = y  
+    z = ""  
+    while i>0 and j > 0: 
+        if s1[i - 1] == s2[j - 1]: 
+            z = s1[i - 1] + z 
+            i -= 1 
+            j -= 1 
+        else:  
+            if dp[i - 1][j] > dp[i][j-1]: i-=1 
+            else: j -=1  
+    return z 
+
+
+# Función para hallar la subsecuencia palíndromo más larga de una cadena
+def longest_palindrome_subsequence(s):
+    # Obtener la longitud de la cadena
+    n = len(s)
+
+    # Crear una matriz LPS de tamaño n x n e inicializarla con ceros
+    LPS = [[0 for _ in range(n)] for _ in range(n)]
+
+    # Recorrer la matriz por diagonales, empezando por la principal
+    for k in range(n):
+        for i in range(n-k):
+            j = i + k
+
+            # Si los caracteres en las posiciones i y j son iguales
+            if s[i] == s[j]:
+                # Si i y j son el mismo carácter o son adyacentes
+                if i == j or i == j - 1:
+                    # El palíndromo tiene longitud 1 o 2
+                    LPS[i][j] = k + 1
+                else:
+                    # El palíndromo tiene longitud mayor que 2
+                    LPS[i][j] = LPS[i+1][j-1] + 2
+            else:
+                # Si los caracteres en las posiciones i y j son diferentes
+                # Se escoge el máximo entre los valores de las celdas adyacentes
+                LPS[i][j] = max(LPS[i+1][j], LPS[i][j-1])
+
+    # La longitud de la subsecuencia palíndromo más larga es LPS[0][n-1]
+    length = LPS[0][n-1]
+
+    # Crear una variable auxiliar para almacenar la subsecuencia palíndromo más larga
+    palindrome = ""
+
+    # Recorrer la matriz LPS en sentido inverso, desde la esquina superior derecha hasta la inferior izquierda
+    i = 0
+    j = n - 1
+    while i <= j:
+        # Si los caracteres en las posiciones i y j son iguales
+        if s[i] == s[j]:
+            # Se añade el carácter a la variable auxiliar
+            palindrome += s[i]
+            # Se avanza a la celda LPS[i+1][j-1]
+            i += 1
             j -= 1
-        else: 
-            if dp[i - 1][j] > dp[i][j-1]: i-=1
-            else: j -=1 
-    return z
+        else:
+            # Si los caracteres en las posiciones i y j son diferentes
+            # Se avanza a la celda LPS[i+1][j] o LPS[i][j-1], según cuál tenga el valor mayor
+            if LPS[i+1][j] > LPS[i][j-1]:
+                i += 1
+            else:
+                j -= 1
+
+    # Si la longitud de la subsecuencia palíndromo más larga es impar
+    if length % 2 == 1:
+        # Se elimina el último carácter de la variable auxiliar, ya que se repite
+        palindrome = palindrome[:-1]
+
+    # Se devuelve la variable auxiliar como la subsecuencia palíndromo más larga, invirtiendo la segunda mitad
+    return palindrome + palindrome[::-1]
+
+
+
 #detecting  plagiarism between documentes 
 def plagiarism_check(file1="contents/Abecedario.txt", file2="contents/AbecedarioCopia.txt"):
     print("Ruta completa del archivo 1:", os.path.abspath(file1))
@@ -40,7 +104,15 @@ def plagiarism_check(file1="contents/Abecedario.txt", file2="contents/Abecedario
         else:
             return "Es poco probable que haya plagio entre los documentos proporcionados, estos tienen una similitud inferior al 50%"
      
-
+def LCS_inmultiplesStrings(cadenas: str):
+    print(cadenas)
+    aux=cadenas.split(',')
+    print(aux)
+    answer=aux[0]
+    for cad in range(len(aux)):
+        answer=lcs(answer,aux[cad])
+    print(answer) 
+    return answer
 
 
 
